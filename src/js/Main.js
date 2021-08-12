9ccfd3d9dd4587609c91c6aad25d1b1892a43be9
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-08-12 09:03:31 -0500

    changes iris to fluidi

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,104 +1,131 @@
-import Component from './BaseComponent';
-import { Router } from 'preact-router';
-import {Helmet} from "react-helmet";
+import Component from "./BaseComponent";
+import { Router } from "preact-router";
+import { Helmet } from "react-helmet";
 
-import Helpers from './Helpers.js';
-import { html } from 'htm/preact';
-import QRScanner from './QRScanner.js';
-import PeerManager from './PeerManager.js';
-import Session from './Session.js';
+import Helpers from "./Helpers.js";
+import { html } from "htm/preact";
+import QRScanner from "./QRScanner.js";
+import PeerManager from "./PeerManager.js";
+import Session from "./Session.js";
 
-import Settings from './views/settings/Settings.js';
-import LogoutConfirmation from './views/LogoutConfirmation.js';
-import Chat from './views/Chat.js';
-import Store from './views/Store.js';
-import Checkout from './views/Checkout.js';
-import Product from './views/Product.js';
-import Login from './views/Login.js';
-import Profile from './views/Profile.js';
-import Group from './views/Group.js';
-import Message from './views/Message.js';
-import Follows from './views/Follows.js';
-import Feed from './views/Feed.js';
-import About from './views/About.js';
-import Explorer from './views/Explorer.js';
-import Contacts from './views/Contacts.js';
-import Torrent from './views/Torrent.js';
+import Settings from "./views/settings/Settings.js";
+import LogoutConfirmation from "./views/LogoutConfirmation.js";
+import Chat from "./views/Chat.js";
+import Store from "./views/Store.js";
+import Checkout from "./views/Checkout.js";
+import Product from "./views/Product.js";
+import Login from "./views/Login.js";
+import Profile from "./views/Profile.js";
+import Group from "./views/Group.js";
+import Message from "./views/Message.js";
+import Follows from "./views/Follows.js";
+import Feed from "./views/Feed.js";
+import About from "./views/About.js";
+import Explorer from "./views/Explorer.js";
+import Contacts from "./views/Contacts.js";
+import Torrent from "./views/Torrent.js";
 
-import Menu from './components/Menu.js';
-import VideoCall from './components/VideoCall.js';
-import MediaPlayer from './components/MediaPlayer.js';
-import Footer from './components/Footer.js';
-import State from './State.js';
+import Menu from "./components/Menu.js";
+import VideoCall from "./components/VideoCall.js";
+import MediaPlayer from "./components/MediaPlayer.js";
+import Footer from "./components/Footer.js";
+import State from "./State.js";
 
-import logoType from '../assets/img/iris_logotype.png';
+import logoType from "../assets/img/iris_logotype.png";
 
-import '../css/style.css';
-import '../css/cropper.min.css';
+import "../css/style.css";
+import "../css/cropper.min.css";
 
-if (window.location.hash && window.location.hash.indexOf('#/') === 0) { // redirect old urls
-  window.location.href = window.location.href.replace('#/', '');
+if (window.location.hash && window.location.hash.indexOf("#/") === 0) {
+  // redirect old urls
+  window.location.href = window.location.href.replace("#/", "");
 }
 
 State.init();
-Session.init({autologin: window.location.pathname.length > 2});
+Session.init({ autologin: window.location.pathname.length > 2 });
 PeerManager.init();
 
 Helpers.checkColorScheme();
 
 class Main extends Component {
   componentDidMount() {
-    State.local.get('loggedIn').on(this.inject());
-    State.local.get('toggleMenu').put(false);
-    State.local.get('toggleMenu').on(show => this.toggleMenu(show));
-    State.electron && State.electron.get('platform').on(this.inject());
-    State.local.get('unseenTotal').on(this.inject());
+    State.local.get("loggedIn").on(this.inject());
+    State.local.get("toggleMenu").put(false);
+    State.local.get("toggleMenu").on((show) => this.toggleMenu(show));
+    State.electron && State.electron.get("platform").on(this.inject());
+    State.local.get("unseenTotal").on(this.inject());
   }
 
   handleRoute(e) {
     let activeRoute = e.url;
-    this.setState({activeRoute});
-    State.local.get('activeRoute').put(activeRoute);
+    this.setState({ activeRoute });
+    State.local.get("activeRoute").put(activeRoute);
     QRScanner.cleanupScanner();
   }
 
   onClickOverlay() {
     if (this.state.showMenu) {
-      this.setState({showMenu: false});
+      this.setState({ showMenu: false });
     }
   }
 
   toggleMenu(show) {
-    this.setState({showMenu: typeof show === 'undefined' ? !this.state.showMenu : show});
+    this.setState({
+      showMenu: typeof show === "undefined" ? !this.state.showMenu : show,
+    });
   }
 
   electronCmd(name) {
-    State.electron.get('cmd').put({name, time: new Date().toISOString()});
+    State.electron.get("cmd").put({ name, time: new Date().toISOString() });
   }
 
   render() {
     let title = "";
     const s = this.state;
     if (s.activeRoute && s.activeRoute.length > 1) {
-      title = Helpers.capitalize(s.activeRoute.replace('/', ''));
+      title = Helpers.capitalize(s.activeRoute.replace("/", ""));
     }
-    let content = '';
-    const isDesktopNonMac = s.platform && s.platform !== 'darwin';
-    const titleTemplate = s.unseenTotal ? `(${s.unseenTotal}) %s | Iris` : "%s | Iris";
-    const defaultTitle = s.unseenTotal ? `(${s.unseenTotal}) Iris` : 'Iris';
+    let content = "";
+    const isDesktopNonMac = s.platform && s.platform !== "darwin";
+    const titleTemplate = s.unseenTotal
+      ? `(${s.unseenTotal}) %s | Iris`
+      : "%s | Iris";
+    const defaultTitle = s.unseenTotal ? `(${s.unseenTotal}) Iris` : "Iris";
     if (s.loggedIn || window.location.pathname.length <= 2) {
-      content = s.loggedIn ? html`
-        ${isDesktopNonMac ? html`
-          <div class="windows-titlebar">
-               <img src=${logoType} height=16 width=28 />
-               <div class="title-bar-btns">
-                    <button class="min-btn" onClick=${() => this.electronCmd('minimize')}>-</button>
-                    <button class="max-btn" onClick=${() => this.electronCmd('maximize')}>+</button>
-                    <button class="close-btn" onClick=${() => this.electronCmd('close')}>x</button>
-               </div>
-          </div>
-        ` : ''}
-        <section class="main ${isDesktopNonMac ? 'desktop-non-mac' : ''} ${s.showMenu ? 'menu-visible-xs' : ''}" style="flex-direction: row;">
+      content = s.loggedIn
+        ? html`
+        ${
+          isDesktopNonMac
+            ? html`
+                <div class="windows-titlebar">
+                  <h1>fluidi</h1>
+                  <div class="title-bar-btns">
+                    <button
+                      class="min-btn"
+                      onClick=${() => this.electronCmd("minimize")}
+                    >
+                      -
+                    </button>
+                    <button
+                      class="max-btn"
+                      onClick=${() => this.electronCmd("maximize")}
+                    >
+                      +
+                    </button>
+                    <button
+                      class="close-btn"
+                      onClick=${() => this.electronCmd("close")}
+                    >
+                      x
+                    </button>
+                  </div>
+                </div>
+              `
+            : ""
+        }
+        <section class="main ${isDesktopNonMac ? "desktop-non-mac" : ""} ${
+            s.showMenu ? "menu-visible-xs" : ""
+          }" style="flex-direction: row;">
           <${Menu}/>
           <${Helmet} titleTemplate=${titleTemplate} defaultTitle=${defaultTitle}>
             <title>${title}</title>
@@ -106,12 +133,16 @@ class Main extends Component {
             <meta property="og:type" content="website" />
             <meta property="og:title" content=${title} />
             <meta property="og:description" content="Social Networking Freedom" />
-            <meta property="og:url" content=${`https://iris.to${window.location.pathname.length > 1 ? window.location.pathname : ''}`} />
+            <meta property="og:url" content=${`https://iris.to${
+              window.location.pathname.length > 1
+                ? window.location.pathname
+                : ""
+            }`} />
             <meta property="og:image" content="https://iris.to/assets/img/cover.jpg" />
           <//>
-          <div class="overlay" onClick=${e => this.onClickOverlay(e)}></div>
+          <div class="overlay" onClick=${(e) => this.onClickOverlay(e)}></div>
           <div class="view-area">
-            <${Router} onChange=${e => this.handleRoute(e)}>
+            <${Router} onChange=${(e) => this.handleRoute(e)}>
               <${Feed} path="/"/>
               <${Feed} path="/feed"/>
               <${Feed} path="/search/:term?/:type?"/>
@@ -143,13 +174,10 @@ class Main extends Component {
         <${MediaPlayer}/>
         <${Footer}/>
         <${VideoCall}/>
-      ` : html`<${Login}/>`;
+      `
+        : html`<${Login} />`;
     }
-    return html`
-      <div id="main-content">
-        ${content}
-      </div>
-    `;
+    return html` <div id="main-content">${content}</div> `;
   }
 }
 
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,24 +1,21 @@
-import Component from '../BaseComponent';
-import Helpers from '../Helpers.js';
-import { html } from 'htm/preact';
-import { translate as t } from '../Translation.js';
-import State from '../State.js';
-import Session from '../Session.js';
-import { route } from 'preact-router';
-import Identicon from './Identicon.js';
-import SearchBox from './SearchBox.js';
-import Icons from '../Icons.js';
-import iris from 'iris-lib';
+import Component from "../BaseComponent";
+import Helpers from "../Helpers.js";
+import { html } from "htm/preact";
+import { translate as t } from "../Translation.js";
+import State from "../State.js";
+import Session from "../Session.js";
+import { route } from "preact-router";
+import Identicon from "./Identicon.js";
+import SearchBox from "./SearchBox.js";
+import Icons from "../Icons.js";
+import iris from "iris-lib";
 
-import logo from '../../assets/img/icon128.png';
-import logoType from '../../assets/img/iris_logotype.png';
-
-import $ from 'jquery';
+import $ from "jquery";
 
 class Header extends Component {
   constructor() {
     super();
-    this.state = {latest: {}};
+    this.state = { latest: {} };
     this.chatId = null;
   }
 
@@ -27,58 +24,85 @@ class Header extends Component {
     const activity = chat && chat.activity;
     if (activity) {
       if (activity.isActive) {
-        return(t('online'));
+        return t("online");
       } else if (activity.lastActive) {
         const d = new Date(activity.lastActive);
-        let lastSeenText = t(iris.util.getDaySeparatorText(d, d.toLocaleDateString({dateStyle:'short'})));
-        if (lastSeenText === t('today')) {
+        let lastSeenText = t(
+          iris.util.getDaySeparatorText(
+            d,
+            d.toLocaleDateString({ dateStyle: "short" })
+          )
+        );
+        if (lastSeenText === t("today")) {
           lastSeenText = iris.util.formatTime(d);
         } else {
           lastSeenText = iris.util.formatDate(d);
         }
-        return (`${t('last_active')  } ${  lastSeenText}`);
+        return `${t("last_active")} ${lastSeenText}`;
       }
     }
   }
 
   backButtonClicked() {
-    route('/chat');
+    route("/chat");
   }
 
   componentDidMount() {
-    State.local.get('showParticipants').on(this.inject());
-    State.local.get('unseenTotal').on(this.inject());
-    State.local.get('activeRoute').on(this.sub(
-      activeRoute => {
-        this.setState({about:null, title: ''});
-        this.setState({activeRoute});
-        const replaced = activeRoute.replace('/chat/new', '').replace('/chat/', '');
+    State.local.get("showParticipants").on(this.inject());
+    State.local.get("unseenTotal").on(this.inject());
+    State.local.get("activeRoute").on(
+      this.sub((activeRoute) => {
+        this.setState({ about: null, title: "" });
+        this.setState({ activeRoute });
+        const replaced = activeRoute
+          .replace("/chat/new", "")
+          .replace("/chat/", "");
         this.chatId = replaced.length < activeRoute.length ? replaced : null;
         if (this.chatId) {
-          State.local.get('channels').get(this.chatId).get('isTyping').on(this.sub(
-            () => this.setState({})
-          ));
-          State.local.get('channels').get(this.chatId).get('theirLastActiveTime').on(this.sub(
-            () => this.setState({})
-          ));
+          State.local
+            .get("channels")
+            .get(this.chatId)
+            .get("isTyping")
+            .on(this.sub(() => this.setState({})));
+          State.local
+            .get("channels")
+            .get(this.chatId)
+            .get("theirLastActiveTime")
+            .on(this.sub(() => this.setState({})));
         }
 
-        if (activeRoute.indexOf('/chat/') === 0 && activeRoute.indexOf('/chat/new') !== 0) {
-          if (activeRoute.indexOf('/chat/') === 0 && Session.getKey() && this.chatId === Session.getKey().pub) {
-            const title = html`<b style="margin-right:5px">📝</b> <b>${t('note_to_self')}</b>`;
-            this.setState({title});
+        if (
+          activeRoute.indexOf("/chat/") === 0 &&
+          activeRoute.indexOf("/chat/new") !== 0
+        ) {
+          if (
+            activeRoute.indexOf("/chat/") === 0 &&
+            Session.getKey() &&
+            this.chatId === Session.getKey().pub
+          ) {
+            const title = html`<b style="margin-right:5px">📝</b>
+              <b>${t("note_to_self")}</b>`;
+            this.setState({ title });
           } else {
-            State.local.get('channels').get(this.chatId).get('name').on(this.inject('title'));
-            State.local.get('channels').get(this.chatId).get('about').on(this.inject());
+            State.local
+              .get("channels")
+              .get(this.chatId)
+              .get("name")
+              .on(this.inject("title"));
+            State.local
+              .get("channels")
+              .get(this.chatId)
+              .get("about")
+              .on(this.inject());
           }
         }
-      }
-    ));
+      })
+    );
   }
 
   onTitleClicked() {
     if (this.chatId) {
-      const view = this.chatId.length < 40 ? '/group/' : '/profile/';
+      const view = this.chatId.length < 40 ? "/group/" : "/profile/";
       route(view + this.chatId);
     }
   }
@@ -86,83 +110,144 @@ class Header extends Component {
   onLogoClick(e) {
     e.preventDefault();
     e.stopPropagation();
-    $('a.logo').blur();
-    ($(window).width() > 625) && route('/');
-    State.local.get('toggleMenu').put(true);
+    $("a.logo").blur();
+    $(window).width() > 625 && route("/");
+    State.local.get("toggleMenu").put(true);
   }
 
   render() {
     const key = Session.getPubKey();
-    if (!key) { return; }
+    if (!key) {
+      return;
+    }
     const activeRoute = this.state.activeRoute;
     const chat = Session.channels[this.chatId];
     const isTyping = chat && chat.isTyping;
-    const onlineStatus = !(chat && chat.uuid) && activeRoute && activeRoute.length > 20 && !isTyping && this.getOnlineStatusText();
-    const searchBox = this.chatId ? '' : html`
-        <${SearchBox} focus=${!!this.state.showMobileSearch}/>
-    `;
-    const chatting = (activeRoute && activeRoute.indexOf('/chat/') === 0);
+    const onlineStatus =
+      !(chat && chat.uuid) &&
+      activeRoute &&
+      activeRoute.length > 20 &&
+      !isTyping &&
+      this.getOnlineStatusText();
+    const searchBox = this.chatId
+      ? ""
+      : html` <${SearchBox} focus=${!!this.state.showMobileSearch} /> `;
+    const chatting = activeRoute && activeRoute.indexOf("/chat/") === 0;
 
-    return html`
-    <header class="nav header">
-      ${activeRoute && activeRoute.indexOf('/chat/') === 0 ? html`
-      <div id="back-button" class="visible-xs-inline-block" onClick=${() => this.backButtonClicked()}>
-        ‹
-      </div>
-      ` : ''}
+    return html` <header class="nav header">
+      ${activeRoute && activeRoute.indexOf("/chat/") === 0
+        ? html`
+            <div
+              id="back-button"
+              class="visible-xs-inline-block"
+              onClick=${() => this.backButtonClicked()}
+            >
+              ‹
+            </div>
+          `
+        : ""}
       <div class="header-content">
-        <div class=${this.state.showMobileSearch ? 'hidden-xs':''}>
-          ${Helpers.isElectron || chatting ? '' : html`
-            <a href="/" onClick=${e => this.onLogoClick(e)} tabindex="0" class="visible-xs-flex logo">
-              <div class="mobile-menu-icon">${Icons.menu}</div>
-              <img src=${logo} style="margin-right: 10px" width=30 height=30/>
-              <img src=${logoType} height=23 width=41 />
-            </a>
-          `}
+        <div class=${this.state.showMobileSearch ? "hidden-xs" : ""}>
+          ${Helpers.isElectron || chatting
+            ? ""
+            : html`
+                <a
+                  href="/"
+                  onClick=${(e) => this.onLogoClick(e)}
+                  tabindex="0"
+                  class="visible-xs-flex logo"
+                >
+                  <div class="mobile-menu-icon">${Icons.menu}</div>
+                  <h1>fluidi</h1>
+                </a>
+              `}
         </div>
-        ${chatting ? '' : html`
-          <div class=${this.state.showMobileSearch ? '' : 'hidden-xs'}>
-            <span class="visible-xs-inline-block" onClick=${() => this.setState({showMobileSearch:false})}>${Icons.close}</span>
-          </div>
-        `}
-        <div class="text" style=${this.chatId ? 'cursor:pointer;text-align:center' : ''} onClick=${() => this.onTitleClicked()}>
-          ${this.state.title && chatting ? html`
-            <div class="name">
-              ${this.state.title}
-            </div>
-          `: ''}
-          ${isTyping ? html`<small class="typing-indicator">${t('typing')}</small>` : ''}
-          ${this.state.about ? html`<small class="participants">${this.state.about}</small>` : ''}
-          ${this.chatId ? html`<small class="last-seen">${onlineStatus || ''}</small>` : ''}
-          ${chatting ? '':html`
-            <div class=${this.state.showMobileSearch ? '' : 'hidden-xs'}>
-              ${searchBox}
-            </div>
-            <div class="mobile-search-btn ${this.state.showMobileSearch ? 'hidden' : 'visible-xs-inline-block'}" onClick=${() => {
-                this.setState({showMobileSearch: true});
-            }}>
-              ${Icons.search}
-            </div>
-          `}
+        ${chatting
+          ? ""
+          : html`
+              <div class=${this.state.showMobileSearch ? "" : "hidden-xs"}>
+                <span
+                  class="visible-xs-inline-block"
+                  onClick=${() => this.setState({ showMobileSearch: false })}
+                  >${Icons.close}</span
+                >
+              </div>
+            `}
+        <div
+          class="text"
+          style=${this.chatId ? "cursor:pointer;text-align:center" : ""}
+          onClick=${() => this.onTitleClicked()}
+        >
+          ${this.state.title && chatting
+            ? html` <div class="name">${this.state.title}</div> `
+            : ""}
+          ${isTyping
+            ? html`<small class="typing-indicator">${t("typing")}</small>`
+            : ""}
+          ${this.state.about
+            ? html`<small class="participants">${this.state.about}</small>`
+            : ""}
+          ${this.chatId
+            ? html`<small class="last-seen">${onlineStatus || ""}</small>`
+            : ""}
+          ${chatting
+            ? ""
+            : html`
+                <div class=${this.state.showMobileSearch ? "" : "hidden-xs"}>
+                  ${searchBox}
+                </div>
+                <div
+                  class="mobile-search-btn ${this.state.showMobileSearch
+                    ? "hidden"
+                    : "visible-xs-inline-block"}"
+                  onClick=${() => {
+                    this.setState({ showMobileSearch: true });
+                  }}
+                >
+                  ${Icons.search}
+                </div>
+              `}
         </div>
 
-        ${chat && this.chatId !== key && !chat.uuid ? html`
-          <a class="tooltip" style="width:24px; height:24px; color: var(--msg-form-button-color)" id="start-video-call" onClick=${() => State.local.get('outgoingCall').put(this.chatId)}>
-            <span class="tooltiptext">${t('video_call')}</span>
-            ${Icons.videoCall}
-          </a>
-          <!-- <a id="start-voice-call" style="width:20px; height:20px; margin-right: 20px">
+        ${chat && this.chatId !== key && !chat.uuid
+          ? html`
+              <a
+                class="tooltip"
+                style="width:24px; height:24px; color: var(--msg-form-button-color)"
+                id="start-video-call"
+                onClick=${() =>
+                  State.local.get("outgoingCall").put(this.chatId)}
+              >
+                <span class="tooltiptext">${t("video_call")}</span>
+                ${Icons.videoCall}
+              </a>
+              <!-- <a id="start-voice-call" style="width:20px; height:20px; margin-right: 20px">
             Icons.voiceCall
             </a> -->
-        `: ''}
-        ${this.chatId && this.chatId.length > 10 && this.chatId.length < 40 ? html`
-          <a class="tooltip hidden-xs" onClick=${() => State.local.get('showParticipants').put(!this.state.showParticipants)}>
-            <span class="tooltiptext">${t('participant_list')}</span>
-            ${Icons.group}
-          </a>
-        ` : ''}
-        <a href="/profile/${key}" onClick=${() => State.local.get('scrollUp').put(true)} class="hidden-xs ${activeRoute && activeRoute === `/profile/${  key}` ? 'active' : ''} my-profile">
-          <${Identicon} str=${key} width=34 />
+            `
+          : ""}
+        ${this.chatId && this.chatId.length > 10 && this.chatId.length < 40
+          ? html`
+              <a
+                class="tooltip hidden-xs"
+                onClick=${() =>
+                  State.local
+                    .get("showParticipants")
+                    .put(!this.state.showParticipants)}
+              >
+                <span class="tooltiptext">${t("participant_list")}</span>
+                ${Icons.group}
+              </a>
+            `
+          : ""}
+        <a
+          href="/profile/${key}"
+          onClick=${() => State.local.get("scrollUp").put(true)}
+          class="hidden-xs ${activeRoute && activeRoute === `/profile/${key}`
+            ? "active"
+            : ""} my-profile"
+        >
+          <${Identicon} str=${key} width="34" />
         </a>
       </div>
     </header>`;
