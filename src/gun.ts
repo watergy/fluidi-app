644c81abf8f62f7a618efed60dd27c44d8e1bc2f
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2021-08-12 06:36:04 -0500

    adds iris

--- src/gun.ts
+++
@@ -1,6 +0,0 @@
-import Gun from "../node_modules/gun";
-import "gun/sea";
-
-const gun = Gun({ peers: ["http://localhost:8765/gun"] });
-
-export default gun;
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export interface Chat {
-  name: string;
-  id: string;
-}
